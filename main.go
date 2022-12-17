84de17e26909b917eb60db6009f3fea7bfa53e14
hikobend <hiko.bkend@gmail.com>
2022-12-17 10:36:15 +0900

    session

--- main.go
+++ main.go
@@ -1,47 +1,41 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/hikobend/login/auth"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
 )
 
-type post struct {
-	Title string `json:"title"`
-	Tag   string `json:"tag"`
-	URL   string `json:"url"`
-}
-
 func main() {
-	r := mux.NewRouter()
-	// localhost:8080/publicでpublicハンドラーを実行
-	r.Handle("/public", public)
-	r.Handle("/private", auth.JwtMiddleware.Handler(private))
-	r.Handle("/auth", auth.GetTokenHandler)
+	r := gin.Default()
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
 
-	//サーバー起動
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
+	r.GET("/hello", func(c *gin.Context) {
+		session := sessions.Default(c)
+
+		if session.Get("hello") != "world" {
+			session.Set("hello", "world")
+			session.Save()
+		}
+
+		c.JSON(200, gin.H{"hello": session.Get("hello")})
+	})
+	r.POST("/login", func(c *gin.Context) {
+		// セッションの作成
+		session := sessions.Default(c)
+		session.Set("loginUser", c.PostForm("userId"))
+		session.Save()
+		c.String(http.StatusOK, "ログイン完了")
+	})
+	r.GET("/logout", func(c *gin.Context) {
+		// セッションの破棄
+		session := sessions.Default(c)
+		session.Clear()
+		session.Save()
+		c.String(http.StatusOK, "ログアウトしました")
+	})
+	r.Run() // listen and server on 0.0.0.0:8080
 }
-
-var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	post := &post{
-		Title: "VueCLIからVue.js入門①【VueCLIで出てくるファイルを概要図で理解】",
-		Tag:   "Vue.js",
-		URL:   "https://qiita.com/po3rin/items/3968f825f3c86f9c4e21",
-	}
-	json.NewEncoder(w).Encode(post)
-})
-
-var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	post := &post{
-		Title: "VGolangとGoogle Cloud Vision APIで画像から文字認識するCLIを速攻でつくる",
-		Tag:   "Go",
-		URL:   "https://qiita.com/po3rin/items/bf439424e38757c1e69b",
-	}
-	json.NewEncoder(w).Encode(post)
-})
